24addff5775c0c030fc7bfeba25960d27d3645cf
ltlaitoff <ltlaitoff@gmail.com>
2023-09-06 18:47:08 +0300

    feat: Import lb1 task

---
+++ src/lb1/6.ts
@@ -0,0 +1,17 @@
+/*
+	Task: 
+	Нехай x = (a1, a2, ... , am) та y = (b1, b2, ... , bn) – пара заданих рядків символів.
+	Визначимо d(x, y) як мінімальне число вставок, видалень і заміна символу, яке
+	необхідно для перетворення x на y.
+	Наприклад: d(ptslddf,tsgldds) = 3
+
+	Для заданих (визначених) x та y знайти d(x, y).
+
+	+ photo https://drive.google.com/file/d/1jBMf_AWINR5s3I4hi-6_XcGPHQLSWg4X/view
+*/
+
+function main() {
+	return 1
+}
+
+export default main
---
+++ src/lb1/8.ts
@@ -0,0 +1,10 @@
+/*
+	Task: 
+	Звести число a в натуральну ступінь n за якомога меншу кількість множень.
+*/
+
+function main() {
+	return 1
+}
+
+export default main
